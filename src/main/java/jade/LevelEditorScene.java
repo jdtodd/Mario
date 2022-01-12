cda0c8e315ad73f93af368c4bc5e0fa810d6288b
jdtodd <todddillan@gmail.com>
2022-01-11 21:07:25 -0600

    Finished Shader compilation and binding

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,34 +1,99 @@
 package jade;
 
+import org.lwjgl.BufferUtils;
+import renderer.Shader;
+
 import java.awt.event.KeyEvent;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class LevelEditorScene extends Scene{
 
-    private boolean changingScene = false;
-    private float timeToChangeScene = 2.0f;
+    private int vertexID, fragmentID, shaderProgram;
+
+    private float[] vertexArray = {
+           // positions           // color
+           0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f, // bottom right
+           -0.5f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f, 1.0f, // top left
+           0.5f, 0.5f,  0.0f,     1.0f, 0.0f, 1.0f, 1.0f, // top right
+          -0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f, // bottom left
+    };
+
+    // IMPORTANT: Must be in counter-clockwise order
+    private int[] elementArray = {
+        2, 1, 0, // top right
+        0, 1, 3  // bottom left
+    };
+
+    private int vaoID, vboID, eboID;
+
+    private Shader defaultShader;
 
     public LevelEditorScene() {
-        System.out.println("Inside Level editor Scene");
+
+    }
+
+    @Override
+    public void init() {
+        defaultShader = new Shader("assets/shaders/default.glsl");
+        defaultShader.compile();
+
+        // Generate VAO, VBO, and EBO buffer objects and send to GPU
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        // Create a float buffer of vertices
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        // Create VBO and upload the vertex buffer
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        // Create the indices and upload
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        // Add the vertex attribute pointers
+        int positionsSize = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+        glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * floatSizeBytes);
+        glEnableVertexAttribArray(1);
     }
 
     @Override
     public void update(float dt) {
+        defaultShader.use();
+        //Bind the VAO
+        glBindVertexArray(vaoID);
 
-        System.out.println("" + (1.0f / dt) + "FPS");
+        // Enable the vertex attribute pointers
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
 
-        if (!changingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)) {
-            changingScene = true;
-        }
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
 
-        if (changingScene && timeToChangeScene > 0) {
-            timeToChangeScene -= dt;
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-        } else if (changingScene) {
-            Window.changeScene(1);
-        }
+        // Unbind everything
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+
+        defaultShader.detach();
     }
-
-
 }
